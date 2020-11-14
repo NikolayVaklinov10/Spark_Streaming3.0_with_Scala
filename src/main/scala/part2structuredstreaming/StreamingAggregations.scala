d5ddde80fa02b7341f5bfde824cc89cc70837f3c
Nick <nikolayvaklinov@yahoo.com>
2020-11-14 19:40:25 +0000

    stream count, numerical aggregation

--- src/main/scala/part2structuredstreaming/StreamingAggregations.scala
+++ src/main/scala/part2structuredstreaming/StreamingAggregations.scala
@@ -1,6 +1,7 @@
 package part2structuredstreaming
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, mean, sum}
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 
 object StreamingAggregations {
 
@@ -9,11 +10,46 @@ object StreamingAggregations {
     .master("local[2]")
     .getOrCreate()
 
-  
+  def streamingCount() = {
+    val lines: DataFrame = spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", 12345)
+      .load()
+
+     val lineCount:DataFrame = lines.selectExpr("count(*) as lineCount")
+
+    // aggregations with distinct are not supported
+    // otherwise Spark will need to keep track of EVERYTHING
+
+    lineCount.writeStream
+      .format("console")
+      .outputMode("complete") // append and update not supported on aggregations without watermark*
+      .start()
+      .awaitTermination()
+  }
+
+  def numericalAggregations(aggFunction: Column => Column): Unit = {
+    val lines: DataFrame = spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", 12345)
+      .load()
+
+    // aggregate here
+    val numbers = lines.select(col("value").cast("integer").as("number"))
+    val aggregationDF = numbers.select(sum(col("number"))).as("agg_so_far")
+
+    aggregationDF.writeStream
+      .format("console")
+      .outputMode("complete")
+      .start()
+      .awaitTermination()
+  }
 
 
   def main(args: Array[String]): Unit = {
-
+    numericalAggregations(mean)
   }
 
 }
