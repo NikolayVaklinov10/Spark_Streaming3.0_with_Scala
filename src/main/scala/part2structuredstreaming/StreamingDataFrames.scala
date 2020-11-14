6be95a4371a76ca73142fe5bce90bce7b7bc19eb
Nick <nikolayvaklinov@yahoo.com>
2020-11-14 16:38:50 +0000

    transformations, reading from files and triggering

--- src/main/scala/part2structuredstreaming/StreamingDataFrames.scala
+++ src/main/scala/part2structuredstreaming/StreamingDataFrames.scala
@@ -1,6 +1,11 @@
 package part2structuredstreaming
 
-import org.apache.spark.sql.{ DataFrame, SparkSession }
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.{col, length}
+import common._
+import org.apache.spark.sql.streaming.Trigger
+
+import scala.concurrent.duration.DurationInt
 
 object StreamingDataFrames {
 
@@ -18,7 +23,13 @@ object StreamingDataFrames {
       .option("port", 12345)
       .load()
 
-    val query = lines.writeStream
+    // transformation
+    val shortLines: DataFrame = lines.filter(length(col("value")) <= 5)
+
+    // tell between a static vs a streaming DF
+    println(shortLines.isStreaming)
+
+    val query = shortLines.writeStream
       .format("console")
       .outputMode("append")
       .start()
@@ -26,8 +37,44 @@ object StreamingDataFrames {
     query.awaitTermination()
   }
 
+  def readFromFiles() = {
+    val stocksDF: DataFrame = spark.readStream
+      .format("csv")
+      .option("header", "false")
+      .option("dateFormat", "MMM d yyyy")
+      .schema(stocksSchema)
+      .load("src/main/resources/data/stocks")
+
+    stocksDF.writeStream
+      .format("console")
+      .outputMode("append")
+      .start()
+      .awaitTermination()
+  }
+
+  def demoTriggers() = {
+    val lines: DataFrame = spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", 12345)
+      .load()
+
+    // write the lines DF at a certain trigger
+    lines.writeStream
+      .format("console")
+      .outputMode("append")
+      .trigger(
+        //Trigger.ProcessingTime(2.seconds) // every 2 seconds run the query
+        //Trigger.Once() // single batch, then terminate
+        Trigger.Continuous(2.seconds) // experimental, every 2 seconds create a batch with whatever you have
+      )
+      .start()
+      .awaitTermination()
+
+  }
+
   def main(args: Array[String]): Unit = {
-    readFromSocket()
+    demoTriggers()
   }
 
 }
