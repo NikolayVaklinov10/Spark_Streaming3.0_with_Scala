fdf1d8e28e0a0def8384ed7b14aca3bf9dfce7c0
Nick <nikolayvaklinov@yahoo.com>
2020-11-14 12:33:34 +0000

    spark.implicits with dollar sign is heavily used in practice

--- src/main/scala/part1recap/SparkRecap.scala
+++ src/main/scala/part1recap/SparkRecap.scala
@@ -1,6 +1,7 @@
 package part1recap
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.col
 
 object SparkRecap extends App {
 
@@ -13,9 +14,18 @@ object SparkRecap extends App {
   val cars = spark.read
     .format("json")
     .option("inferSchema", "true")
-    .load("src/main/resources/data/car")
+    .load("src/main/resources/data/cars")
 
+  // showing a DF to the console
   cars.printSchema()
   cars.show()
+
+  // widely used in practice
+  import spark.implicits._
+  // the select function
+  cars.select(
+    col("Name"), // column object
+    $"Year" // another column object
+  )
 
 }
