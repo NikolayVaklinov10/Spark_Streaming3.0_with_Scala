1eae950b8009acd1834b1f4e87cff0154681d2ed
Nick <nikolayvaklinov@yahoo.com>
2020-11-14 22:32:07 +0000

    the converted df -> ds

--- src/main/scala/part2structuredstreaming/StreamingDatasets.scala
+++ src/main/scala/part2structuredstreaming/StreamingDatasets.scala
@@ -1,6 +1,8 @@
 package part2structuredstreaming
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import common._
 
 object StreamingDatasets {
 
@@ -8,6 +10,21 @@ object StreamingDatasets {
     .appName("Streaming Datasets")
     .master("local[2]")
     .getOrCreate()
+
+  // the implicit needed for the conversion
+  import spark.implicits._
+
+  def readCars() = {
+    spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", 12345)
+      .load() // DF with single string column "value"
+      .select(from_json(col("value"), carsSchema).as("car")) // composite column (struct)
+      .selectExpr("car.*") // DF with multiple columns
+      .as[Car]
+
+  }
 
 
 
