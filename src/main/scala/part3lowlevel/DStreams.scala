bb8a9e736a3f0813875cafd2b765543fed2356d0
Nick <nikolayvaklinov@yahoo.com>
2020-11-15 10:25:34 +0000

    reading DStreams from sockets

--- src/main/scala/part3lowlevel/DStreams.scala
+++ src/main/scala/part3lowlevel/DStreams.scala
@@ -1,6 +1,7 @@
 package part3lowlevel
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object DStreams {
@@ -21,14 +22,28 @@ object DStreams {
   /*
   - define input sources by creating DStreams
   - define transformations on DStreams
+  - call an actions on DStreams
   - start ALL computations with ssc.start()
   - no more computations can be added
   - await termination, or stop the computation
   - you cannot restart the ssc
    */
 
-  def main(args: Array[String]): Unit = {
+  def readFromSocket() = {
+    val socketStream: DStream[String] = ssc.socketTextStream("localhost", 12345)
 
+    // transformation = lazy
+    val wordsStream: DStream[String] = socketStream.flatMap(line => line.split(" "))
+
+    // action
+    wordsStream.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+  def main(args: Array[String]): Unit = {
+    readFromSocket()
   }
 
 }
