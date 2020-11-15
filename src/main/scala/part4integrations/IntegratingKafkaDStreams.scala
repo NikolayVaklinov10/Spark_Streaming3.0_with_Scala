d616f78da89bccd1f61e2e61b87dd5962b536f52
Nick <nikolayvaklinov@yahoo.com>
2020-11-15 21:16:02 +0000

    the write to kafka method

--- src/main/scala/part4integrations/IntegratingKafkaDStreams.scala
+++ src/main/scala/part4integrations/IntegratingKafkaDStreams.scala
@@ -1,7 +1,11 @@
 package part4integrations
 
+import java.util
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object IntegratingKafkaDStreams {
@@ -23,10 +27,71 @@ object IntegratingKafkaDStreams {
     "enable.auto.commit" -> false.asInstanceOf[Object]
   )
 
+  val kafkaTopic = "rockthejvm"
+
+  def readFromKafka() = {
+    val topics = Array(kafkaTopic)
+    val kafkaDStream = KafkaUtils.createDirectStream(
+      ssc,
+      LocationStrategies.PreferConsistent,
+      /*
+       Distributes the partitions evenly across the Spark cluster.
+       Alternatives:
+       - PreferBrokers if the brokers and executors are in the same cluster
+       - PreferFixed
+      */
+      ConsumerStrategies.Subscribe[String, String](topics, kafkaParams + ("group.id" -> "group1"))
+      /*
+        Alternative
+        - SubscribePattern allows subscribing to topics matching a pattern
+        - Assign - advanced; allows specifying offsets and partitions per topic
+       */
+    )
+
+    val processedStream = kafkaDStream.map(record => (record.key(), record.value()))
+    processedStream.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+  def writeToKafka() = {
+    val inputData = ssc.socketTextStream("localhost", 12345)
+
+    // transform data
+    val processedData = inputData.map(_.toUpperCase())
+
+    processedData.foreachRDD { rdd =>
+      rdd.foreachPartition { partition =>
+        // inside this lambda, the code is run by a single executor
+
+        val kafkaHashMap = new util.HashMap[String, Object]()
+        kafkaParams.foreach { pair =>
+          kafkaHashMap.put(pair._1, pair._2)
+        }
+
+        // producer can insert records into the Kafka topics
+        // available on this executor
+        val producer = new KafkaProducer[String, String](kafkaHashMap)
+
+        partition.foreach { value =>
+          val message = new ProducerRecord[String, String](kafkaTopic, null, value)
+          // feed the message into the Kafka topic
+          producer.send(message)
+        }
+
+        producer.close()
+      }
+    }
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
 
 
   def main(args: Array[String]): Unit = {
-
-  }
+      readFromKafka()
+    }
 
 }
