91fee078b0d189b88ca46dff0c691f39ecd7505a
Nick <nikolayvaklinov@yahoo.com>
2020-11-16 15:10:52 +0000

    read purchases from socket and file functions

--- src/main/scala/part5advanced/EventTimeWindows.scala
+++ src/main/scala/part5advanced/EventTimeWindows.scala
@@ -1,6 +1,7 @@
 package part5advanced
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, from_json}
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType, TimestampType}
 
 object EventTimeWindows {
@@ -16,6 +17,19 @@ object EventTimeWindows {
     StructField("item", StringType),
     StructField("quantity", IntegerType)
   ))
+
+  def readPurchasesFromSocket() = spark.readStream
+    .format("socket")
+    .option("host", "localhost")
+    .option("port", 12345)
+    .load()
+    .select(from_json(col("value"), onlinePurchaseSchema).as("purchase"))
+    .selectExpr("purchase.*")
+
+  def readPurchasesFromFile() = spark.readStream
+    .schema(onlinePurchaseSchema)
+    .json("src/main/resources/data/purchases")
+
 
 
 
